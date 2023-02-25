6bd8f1219662d2dd9177c3fcdbebb08053b0413f
Waqas Tahir <wakaztahir@gmail.com>
2023-02-25 14:24:08 +0500

    fix jvm file resource to use only forward slashes

--- resources/src/jvmMain/kotlin/dev/icerock/moko/resources/FileResource.kt
+++ resources/src/jvmMain/kotlin/dev/icerock/moko/resources/FileResource.kt
@@ -11,8 +11,10 @@ actual open class FileResource(
     val filePath: String
 ) {
     fun readText(): String {
-        val stream = resourcesClassLoader.getResourceAsStream(filePath)
-            ?: throw FileNotFoundException("Couldn't open resource as stream at: $filePath")
+        // https://github.com/icerockdev/moko-resources/issues/424
+        val jvmFilePath = filePath.replace('\\', '/')
+        val stream = resourcesClassLoader.getResourceAsStream(jvmFilePath)
+            ?: throw FileNotFoundException("Couldn't open resource as stream at: $jvmFilePath")
         return stream.use { it.readBytes().decodeToString() }
     }
 }
