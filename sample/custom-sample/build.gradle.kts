34be493514c70de533900ec1f4723e958fe1c654
Waqas Tahir <wakaztahir@gmail.com>
2023-03-06 22:21:15 +0500

    custom sample added

---
+++ sample/custom-sample/build.gradle.kts
@@ -0,0 +1,84 @@
+import java.util.Properties
+
+plugins {
+    kotlin("multiplatform")
+    id("maven-publish")
+    id("org.jetbrains.compose")
+    id("com.android.library")
+    id("plugin.multiplatform-resources")
+}
+
+multiplatformResources {
+    multiplatformResourcesPackage = "com.something.some.ui" // required
+    multiplatformResourcesClassName = "Res" // optional, default MR
+    multiplatformResourcesVisibility = dev.icerock.gradle.MRVisibility.Internal // optional, default Public
+    iosBaseLocalizationRegion = "en" // optional, default "en"
+    multiplatformResourcesSourceSet = "commonMain"  // optional, default "commonMain"
+}
+
+kotlin {
+    android()
+    jvm("desktop") {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+    }
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(compose.runtime)
+                api(compose.foundation)
+                api(projects.resources)
+                api(projects.resourcesCompose)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+
+            }
+        }
+//        val androidTest by getting {
+//            dependencies {
+//                implementation("junit:junit:4.13.2")
+//            }
+//        }
+        val desktopMain by getting {
+            dependencies {
+                api(compose.preview)
+            }
+        }
+        val desktopTest by getting
+
+        named("jsMain") {
+            dependencies {
+                api(compose.web.core)
+            }
+        }
+    }
+}
+
+android {
+    compileSdk = 32
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].apply {
+        res.srcDir(File(buildDir, "generated/moko/androidMain/res"))
+    }
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 32
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
